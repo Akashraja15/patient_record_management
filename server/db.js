d6c8592d61a2f3d5fb02ea9c434daa9140da4964
Akash Raja D <akas2989@global.chainsys.com>
2022-06-06 14:33:58 +0530

    included files

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ var password = "68fc5b9dc8c58071087abaecc44a5f29";
 var cloudant = Cloudant({ url: url, username: username, password: password });
 
  
-insert = function (paramsvalue) {
+var insert = function (paramsvalue) {
   console.log(paramsvalue);
   cloudant
     .use("freshers_sample")
@@ -19,13 +19,13 @@ insert = function (paramsvalue) {
       console.log(err);
     });
 };
-get = function (admindata,dbname) {
+var get = function (admindata,dbname) {
   return cloudant.use(dbname).find(admindata);
 };
-getId = function (id, dbfname) {
+var getId = function (id, dbfname) {
   return cloudant.use(dbfname).get(id);
 };
-del_id = function (id, id1, dbname) {
+var del_id = function (id, id1, dbname) {
   return cloudant.use(dbname).destroy(id, id1);
 };
 module.exports = { get, getId, insert, del_id };
