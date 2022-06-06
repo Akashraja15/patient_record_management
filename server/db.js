743f28d29301dca0f796cba877adde4d0e0c4507
Akash Raja D <akas2989@global.chainsys.com>
2022-06-06 18:51:05 +0530

    included files

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const Cloudant = require("@cloudant/cloudant");
-var url =
+const url =
 "https://2fbcb9ec-d57d-431a-8d72-186d88ddf478-bluemix.cloudantnosqldb.appdomain.cloud"
-var username = "apikey-v2-kf8ex4frj52lu2wwin72qqktpi3occ9bfv4p80vbr99";
-var password = "68fc5b9dc8c58071087abaecc44a5f29";
+const username = "apikey-v2-kf8ex4frj52lu2wwin72qqktpi3occ9bfv4p80vbr99";
+const password = "68fc5b9dc8c58071087abaecc44a5f29";
  
-var cloudant = Cloudant({ url: url, username: username, password: password });
+const cloudant = Cloudant({ url: url, username: username, password: password });
 
  
 insert = function (paramsvalue) {
